aef239bdf85ca4b22270951477eb6662fb788a37
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-24 03:05:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,14 +7,10 @@ end
 (id_generator) .to be_a Hash
 end
 (my_hash_creator(:name, 'Grace Hopper')) .to be_a(Hash)
-
 end
-
-def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+(read_from_hash({name: 'Steve'}, :name )) .to eq('Steve')
 end
-
-def update_counting_hash(hash, key)
+(update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
